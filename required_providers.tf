f2ee4a977215b60d6c473f12d34a6b21200542f1
Alonso Suarez <alonso.suarez@jane.app>
2025-05-21 10:43:39 -0700

    fix backend

--- required_providers.tf
+++ required_providers.tf
@@ -15,3 +15,15 @@ terraform {
   }
   required_version = ">= 1.0"
 }
+
+terraform {
+  backend "s3" {
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  # Set to us-east-1 as this is where resources have to live
+  # for the ACM certificate to be attached to CloudFront
+  region = "us-east-1"
+}
